8c6973ae45a54c3a89115a0ebc9c4e1ea1bb5d33
Alefsandler Felisberto <aleffelisberto@gmail.com>
2022-04-30 08:20:04 -0300

    add jwt authentication

--- index.js
+++ index.js
@@ -1,59 +1,98 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
+const cors = require('cors')
+
+const JWT_KEY = 'aI9#0F89%$9F0@-2DSF#$1373#Ssd'
 
 const app = express()
+
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// mocky database
-let games = [
-  {
-    id: 1,
-    title: 'Sea of Thieves',
-    year: 2018,
-    price: 159.9
-  },
-  {
-    id: 2,
-    title: 'Assassins Creed II',
-    year: 2009,
-    price: 39.9
-  },
-  {
-    id: 3,
-    title: 'Resident Evil 6',
-    year: 2012,
-    price: 59.9
-  },
-  {
-    id: 4,
-    title: 'Pes 2022',
-    year: 2022,
-    price: 199.9
-  },
-  {
-    id: 5,
-    title: 'Red Dead Redemption',
-    year: 2018,
-    price: 149.9
+// middleware
+const auth = (req, res, next) => {
+  let authToken = req.headers['authorization']
+  if (authToken !== undefined) {
+    authToken = authToken.replace('Bearer ', '')
+    jwt.verify(authToken, JWT_KEY, (err, data) => {
+      if (err) {
+        res.status(401)
+        res.json({ err: 'Token inválido' })
+      } else {
+        req.token = authToken
+        req.loggedUser = { id: data.id, email: data.email }
+        next()
+      }
+    })
+  } else {
+    res.status(401)
+    res.json({ error: 'Você enviou o token ?' })
   }
-]
+}
+
+// mocky database
+let DB = {
+  games: [
+    {
+      id: 1,
+      title: 'Sea of Thieves',
+      year: 2018,
+      price: 159.9
+    },
+    {
+      id: 2,
+      title: 'Assassins Creed II',
+      year: 2009,
+      price: 39.9
+    },
+    {
+      id: 3,
+      title: 'Resident Evil 6',
+      year: 2012,
+      price: 59.9
+    },
+    {
+      id: 4,
+      title: 'Pes 2022',
+      year: 2022,
+      price: 199.9
+    },
+    {
+      id: 5,
+      title: 'Red Dead Redemption',
+      year: 2018,
+      price: 149.9
+    }
+  ],
+  users: [
+    { id: 1, email: 'sandrinfc@lubmail.com', password: 'sandrinfc' },
+    { id: 2, email: 'durabolinFC@duramail.com', password: 'durateston' }
+  ]
+}
 
 // return all games
-app.get('/games', (req, res) => {
+app.get('/games', auth, (req, res) => {
   res.statusCode = 202
-  res.json(games)
+  res.json({
+    user: req.loggedUser,
+    games: DB.games
+  })
 })
 
 // return a specified game by its id
-app.get('/game/:id', (req, res) => {
+app.get('/game/:id', auth, (req, res) => {
   const { id } = req.params
   if (!isNaN(id)) {
-    const result = games.filter(game => game.id === parseInt(id))
+    const result = DB.games.filter(game => game.id === parseInt(id))
     console.log(result)
     if (result.length > 0) {
       res.statusCode = 202
-      res.json(result)
+      res.json({
+        user: req.loggedUser,
+        game: result
+      })
     } else {
       res.sendStatus(404)
     }
@@ -65,13 +104,13 @@ app.get('/game/:id', (req, res) => {
 })
 
 // create a game
-app.post('/game', (req, res) => {
+app.post('/game', auth, (req, res) => {
   const { title, year, price } = req.body
 
   if (title === undefined || year === undefined || price === undefined) {
     res.sendStatus(400)
   } else {
-    const id = games[games.length - 1].id + 1
+    const id = DB.games[games.length - 1].id + 1
     games.push({
       id,
       title,
@@ -82,14 +121,38 @@ app.post('/game', (req, res) => {
   }
 })
 
+app.post('/auth', auth, (req, res) => {
+  const { email, password } = req.body
+
+  if (email != undefined) {
+    const user = DB.users.find(user => user.email === email)
+    if (user !== undefined) {
+      if (password === user.password) {
+        //payload
+        const token = jwt.sign({ id: user.id, email: user.email }, JWT_KEY, {
+          expiresIn: '48h'
+        })
+        res.status(200)
+        res.json({ token: token })
+      }
+    } else {
+      res.status(404)
+      res.json({ password: 'senha inválida' })
+    }
+  } else {
+    res.status(400)
+    res.send({ error: 'O email enviado é inválido' })
+  }
+})
+
 // delete a game
-app.delete('/game/:id', (req, res) => {
+app.delete('/game/:id', auth, (req, res) => {
   const { id } = req.params
 
   if (isNaN(id)) {
     res.sendStatus(400)
   } else {
-    const gameIndex = games.findIndex(element => element.id === parseInt(id))
+    const gameIndex = DB.games.findIndex(element => element.id === parseInt(id))
     if (id === -1) res.sendStatus(404)
     else {
       games.splice(gameIndex)
@@ -99,7 +162,7 @@ app.delete('/game/:id', (req, res) => {
 })
 
 // edit a game info
-app.put('/game/:id', (req, res) => {
+app.put('/game/:id', auth, (req, res) => {
   const { id } = req.params
   const { title, year, price } = req.body
 
@@ -115,7 +178,7 @@ app.put('/game/:id', (req, res) => {
   if (isInvalid) {
     res.sendStatus(400)
   } else {
-    const game = games.find(game => game.id === parseInt(id))
+    const game = DB.games.find(game => game.id === parseInt(id))
 
     if (game) {
       game.year = year
